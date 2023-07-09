ffe807c73219c65caeeb521763b0918758082011
Roy Lopez <roylopezdev@gmail.com>
2023-07-09 15:06:46 -0300

    create prompt schema

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -11,8 +11,30 @@ const Createprompt = () => {
     prompt: "",
     tag: "",
   });
+  const router = useRouter();
 
-  const createPrompt = async e => {};
+  const createPrompt = async e => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+        setPost({ prompt: "", tag: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
