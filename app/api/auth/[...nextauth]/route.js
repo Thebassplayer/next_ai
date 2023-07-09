2bdc6d45c58e879c0e8ab5bf3999bea0843dd79e
Roy Lopez <roylopezdev@gmail.com>
2023-07-09 14:50:58 -0300

    Create prompt form component

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // Check if the user already exists in the database
+        const userExist = await User.findOne({ email: profile.email });
+
+        // If the user does not exist, create a new user
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            Image: profile.picture,
+          });
+        }
+
+        return true;
+
+        // If the user exists, return true to sign in
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+
+      session.user.id = sessionUser?._id.toString();
+
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,17 +7,17 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  // useEffect(() => {
-  //   const setProviders = async () => {
-  //     const response = await getProviders();
-  //     setProviders(response);
-  //   };
-  //   setProviders();
-  // }, []);
+  useEffect(() => {
+    const setUpProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setUpProviders();
+  }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -30,8 +30,10 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+
+      {/* Desktop Nav */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -41,7 +43,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user?.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -65,10 +67,10 @@ const Nav = () => {
       </div>
       {/* Mobile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user?.image}
               width={37}
               height={37}
               className="rounded-full cursor-pointer"
@@ -113,10 +115,12 @@ const Nav = () => {
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
