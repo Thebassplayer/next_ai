2efdf09caf13f21332b1ac1150a0e9dba4b1e075
Roy Lopez <roylopezdev@gmail.com>
2023-07-11 18:59:02 -0300

    Create useCreatePrompt custom Hook

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,40 +2,47 @@
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import useCreatePrompt from "@hooks/useCreatePrompt";
 
 import Form from "@components/Post";
 
 const CreatePrompt = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
+  // const router = useRouter();
+  // const { data: session } = useSession();
 
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+  // const [submitting, setSubmitting] = useState(false);
+  // const [post, setPost] = useState({
+  //   prompt: "",
+  //   tag: "",
+  // });
 
-  const createPrompt = async e => {
+  // const createPrompt = async e => {
+  //   e.preventDefault();
+  //   setSubmitting(true);
+  //   try {
+  //     const response = await fetch("/api/prompt/new", {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         prompt: post.prompt,
+  //         userId: session?.user.id,
+  //         tag: post.tag,
+  //       }),
+  //     });
+  //     if (response.ok) {
+  //       router.push("/");
+  //       setPost({ prompt: "", tag: "" });
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setSubmitting(false);
+  //   }
+  // };
+
+  const { post, setPost, submitting, createPrompt } = useCreatePrompt();
+  const handleSubmit = e => {
     e.preventDefault();
-    setSubmitting(true);
-    try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag,
-        }),
-      });
-      if (response.ok) {
-        router.push("/");
-        setPost({ prompt: "", tag: "" });
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setSubmitting(false);
-    }
+    createPrompt();
   };
 
   return (
@@ -44,7 +51,7 @@ const CreatePrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={createPrompt}
+      handleSubmit={handleSubmit}
     />
   );
 };
---
+++ hooks/useCreatePrompt.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const useCreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const [submitting, setSubmitting] = useState(false);
+  const createPrompt = async () => {
+    setSubmitting(true);
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+        setPost({ prompt: "", tag: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return { post, setPost, submitting, createPrompt };
+};
+
+export default useCreatePrompt;
