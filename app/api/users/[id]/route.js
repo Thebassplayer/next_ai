5ac73f664c817fbbd31e4a713f2e9cb3dc86bb0a
Roy Lopez <roylopezdev@gmail.com>
2023-07-11 17:18:13 -0300

    update users route

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -8,21 +8,6 @@ export const PATCH = async (req, { params }) => {
   try {
     await connectToDB();
 
-    const existingPrompt = await Prompt.findById(params.id);
-
-    if (!existingPrompt) {
-      return new Response("Prompt not found", {
-        status: 404,
-      });
-    }
-
-    existingPrompt.prompt = prompt;
-    existingPrompt.tag = tag;
-    existingPrompt.favourite = favourite;
-    existingPrompt.shared = shared;
-
-    await existingPrompt.save();
-
     return new Response(JSON.stringify(existingPrompt), {
       status: 200,
     });
