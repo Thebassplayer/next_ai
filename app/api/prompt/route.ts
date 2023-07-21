e705d2a108b7a2c77b9ae3b1569d5ccae091f8fa
Roy Lopez <roylopezdev@gmail.com>
2023-07-21 18:13:18 -0300

    type prompt/new route

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,13 +1,15 @@
 import { connectToDB } from "@utils/database";
+import { NextApiRequest, NextApiResponse } from "next";
 
 import Prompt from "@models/prompt";
 
-export const GET = async req => {
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), {
+    const posts = await Prompt.find({}).populate("creator");
+
+    return new Response(JSON.stringify(posts), {
       status: 200,
     });
   } catch (error) {
