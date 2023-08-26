5904c6c1c8229b283cf1b8a6a2119d619b63e9f0
Roy Lopez <roylopezdev@gmail.com>
2023-08-26 08:46:03 -0300

    update middleware policies

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,32 @@
-import { withAuth } from "next-auth/middleware";
-import { getServerSession } from "next-auth/next";
-import { NextResponse, NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(request: NextRequest) {
-    console.log("middleware");
-    console.log(request.url);
-    console.log(request.method);
-  },
-  {
-    callbacks: {
-      authorized: ({ req, token }) => {
-        if (!token) {
-          console.log("not authorized");
-          return false;
-        }
-        console.log("authorized");
-        return true;
-      },
-    },
-  }
-);
+export const config = { matcher: ["/:path*"] };
 
-export const config = {
-  matcher: ["/api/:path*", "/user/:path*"],
-};
+// import { withAuth } from "next-auth/middleware";
+// import { getServerSession } from "next-auth/next";
+// import { NextResponse, NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
+
+// export default withAuth(
+//   function middleware(request: NextRequest) {
+//     console.log("middleware");
+//     console.log(request.url);
+//     console.log(request.method);
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ req, token }) => {
+//         if (!token) {
+//           console.log("not authorized");
+//           return false;
+//         }
+//         console.log("authorized");
+//         return true;
+//       },
+//     },
+//   }
+// );
+
+// export const config = {
+//   matcher: ["/api/:path*", "/user/:path*"],
+// };
