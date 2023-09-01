5ce2ec8332e79c0c686d62cce24e4f1336dd88cd
Roy Lopez <roylopezdev@gmail.com>
2023-09-01 15:30:27 -0300

    fix callback config bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,15 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
   callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
+
+      session.user.id = sessionUser?._id.toString();
+
+      return session;
+    },
     async signIn({ profile }) {
       try {
         if (!profile.email) throw new Error("No email found");
@@ -39,13 +47,6 @@ const handler = NextAuth({
         console.log(error);
         return false;
       }
-    },
-    async session({ session }) {
-      const sessionUser = await User.findOne({ email: session.user.email });
-
-      session.user.id = sessionUser?._id.toString();
-
-      return session;
     },
   },
   pages: {
