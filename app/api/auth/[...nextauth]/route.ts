16598cef67bc224f28c8e378921caefb5cc4a783
Roy Lopez <roylopezdev@gmail.com>
2023-09-07 17:00:21 -0300

    fix login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,8 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-// Mongo DB
+
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
-import { generateUniqueUsername } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
@@ -12,47 +11,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-
   callbacks: {
     async session({ session }) {
+      // store the user id from MongoDB to session
       const sessionUser = await User.findOne({ email: session.user.email });
-
-      session.user.id = sessionUser?._id.toString();
+      session.user.id = sessionUser._id.toString();
 
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
-        if (!profile.email) throw new Error("No email found");
-
         await connectToDB();
 
-        // Check if the user already exists in the database
-        const userExist = await User.findOne({ email: profile.email });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-        // If the user does not exist, create a new user
-        if (!userExist) {
-          const newUser = await generateUniqueUsername();
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
           await User.create({
             email: profile.email,
-            username: newUser,
+            username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture,
           });
         }
 
         return true;
-
-        // If the user exists, return true to sign in
       } catch (error) {
-        console.log(error);
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
-  },
-  pages: {
-    signIn: "/",
-    signOut: "/",
-    error: "/auth/error",
   },
 });
 
