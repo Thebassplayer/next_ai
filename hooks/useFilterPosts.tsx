f3c170593382fceb4d4348b731cf558cbd37c2f1
Roy Lopez <roylopezdev@gmail.com>
2023-09-10 16:23:21 -0300

    Fix bug when no user exists

--- hooks/useFilterPosts.tsx
+++ hooks/useFilterPosts.tsx
@@ -24,7 +24,7 @@ const useFilterPosts = ({
     const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive search
     return allPosts.filter(
       item =>
-        regex.test(item.creator.username) ||
+        regex.test(item?.creator?.username) ||
         regex.test(item.tag) ||
         regex.test(item.prompt)
     );
