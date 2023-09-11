24e6d79d9503bb408cca5ed1d800dbc7b9d7a702
Roy Lopez <roylopezdev@gmail.com>
2023-09-11 15:32:47 -0300

    fix useFavoritePosts

--- app/components/Favorites.tsx
+++ app/components/Favorites.tsx
@@ -3,23 +3,9 @@
 import useFavoritePosts from "@hooks/useFavouritePosts";
 import PostCard from "./PostCard";
 import Loading from "./Loading";
-import { fetchFavorites } from "fetchData/fetchData";
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
-import { useMemo } from "react";
 
 const Favorites = () => {
-  const { data: session } = useSession();
-  const userId = useMemo(() => session?.user?.id, [session]);
-
-  const {
-    data: favoritePosts,
-    isLoading,
-    isSuccess,
-  } = useQuery({
-    queryKey: ["favorites"],
-    queryFn: () => fetchFavorites(userId),
-  });
+  const { favoritePosts, isLoading, isSuccess } = useFavoritePosts();
 
   return (
     <section className="w-full">
